db6bba31b5b23f64546dbb101979a583eb4fd0ad
Nelson Bryan <201913582B@utea.edu.pe>
2022-10-19 09:47:00 -0500

    Formulario para ingresar

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php Include 'complementos/header.php'?>
+
+
+<div class="container">
+    <div class="row mt-3 justify-content-md-center">
+        <div class="col-md-6">
+            <form action="" method="POST">
+
+                <div class="form-group">
+                    <label for="username">Email:</label>
+                    <input class="form-control"name="username"type="text" placeholder="Ingresa tu email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Contraseña:</label>
+                    <input class="form-control"name="password"type="password" placeholder="Ingresa tu contraseña">
+                </div>
+
+                <button type="submit" class="btn btn-sm btn-block btn-primary">Iniciar Sesion</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<?php Include 'complementos/footer.php'?>
